224225e4326708a4695233903e8580383dbc3e82
Maja Vasilic <rmaja7@gmail.com>
2018-02-09 14:36:50 +0100

    Maja pravi sranja

---
+++ src/Matrice.java
@@ -0,0 +1,26 @@
+public class Matrice {
+
+	public static void main(String[] args) {
+
+	}
+
+	/***/
+	public static int[] eliminateDuplicates(int[] list) {
+	}
+
+	public static boolean isSorted(int[] list) {
+
+	}
+
+	public static String sort(String s) {
+	}
+
+	public static double sumMajorDiagonal(double[][] m) {
+	}
+
+	public static double sumColumn(double[][] m, int columnIndex) {
+	}
+
+	public static void sort(int m[][]) {
+	}
+}
