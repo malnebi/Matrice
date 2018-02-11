6c63d9c41b892c124e13b6d8b86dd8225e75faf3
Maja Vasilic <rmaja7@gmail.com>
2018-02-11 02:02:55 +0100

    kostur projekta

--- src/Matrice.java
+++ src/Matrice.java
@@ -4,23 +4,66 @@ public class Matrice {
 
 	}
 
-	/***/
+	/**
+	 * @author Vanja Bijelovic
+	 */
+	/**
+	 * 1.Method returns a new array by eliminating the duplicate values in the
+	 * array.
+	 */
 	public static int[] eliminateDuplicates(int[] list) {
 	}
 
+	/**
+	 * @author Vanja Bijelovic
+	 */
+	/**
+	 * 2. Method returns true if the list is already sorted in increasing order.
+	 */
 	public static boolean isSorted(int[] list) {
-
 	}
 
+	/**
+	 * @author Vanja Mihajlovic
+	 */
+
+	/**
+	 * 3.Method returns a sorted string For example, sort("acb") returns abc.
+	 */
 	public static String sort(String s) {
 	}
 
-	public static double sumMajorDiagonal(double[][] m) {
-	}
+	/**
+	 * @author Vanja Mihajlovic
+	 */
 
+	/**
+	 * 4. Method that sums all the numbers in the major diagonal in an n * n
+	 * matrix of double values using the following header: public static double
+	 * sumMajorDiagonal(double[][] m) Write a test program that reads a 4-by-4
+	 * matrix and displays the sum of all its elements on the major diagonal.
+	 */
+
+	/**
+	 * @author Maja Vasilic
+	 */
+
+	/**
+	 * 6. Method returns the sum of all the elements in a specified column in a
+	 * matrix
+	 */
 	public static double sumColumn(double[][] m, int columnIndex) {
 	}
 
+	/**
+	 * @author Maja Vasilic
+	 */
+
+	/**
+	 * 7. Method sorts a two-dimensional array The method performs a primary
+	 * sort on rows and a secondary sort on columns.
+	 */
 	public static void sort(int m[][]) {
 	}
+
 }
